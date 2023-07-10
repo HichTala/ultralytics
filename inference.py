0260d1f91a931219fb3bbe1b9f5c37a695853cf3
HichTala <98521878+HichTala@users.noreply.github.com>
2023-07-10 18:38:02 +0200

    Advencement on clustering

--- inference.py
+++ inference.py
@@ -2,57 +2,95 @@ import cv2
 import numpy as np
 from sklearn.cluster import KMeans
 from PIL import Image
+import binascii
 
+import scipy
+import scipy.misc
+import scipy.cluster
 from ultralytics import YOLO
 
-model = YOLO("./runs/detect/train14/weights/best.pt")
+model = YOLO("./runs/YOLO_YuGiOh_300eps_300523/train14/weights/best.pt")
 
 # metrics = model.val()  # evaluate model performance on the validation set
 results = model(
-    source="/home/cose-ia/Downloads/YuGiOh_YOLO.v2i.yolov8/video/2023-07-02 16-38-07.mp4",
+    source="./Videos/2023-07-02 16-26-49 (online-video-cutter.com).mp4",
     show_labels=False,
     save=False
 )
 
-clusters = 5
+clusters = 10
 
 kmeans = KMeans(n_clusters=clusters, n_init='auto')
 
-colors = {
-    'spell': np.array([75, 120, 120]),
-    'trap': np.array([110, 50, 90]),
-    'monster': np.array([164, 113, 38]),
-    'link': np.array([38, 38, 165]),
-}
-fourcc = cv2.VideoWriter_fourcc(*"MP4V")
-video_writer = cv2.VideoWriter('filename.mp4', fourcc, 10, results[0].orig_shape, 3)
+# colors = {
+#     'spell': np.array([75, 120, 120]),
+#     'monster': np.array([164, 113, 38]),
+#     'link': np.array([38, 38, 165]),
+#     'xyz': np.array([0, 0, 0]),
+#     'syncro': np.array([150, 150, 175]),
+# }
 
+colors = [
+    np.array([0, 113, 112]),
+    np.array([115, 76, 63]),
+    np.array([51, 112, 150]),
+    np.array([25, 49, 68]),
+    np.array([165, 173, 180]),
+]
 
+card_types = ['spell', 'monster', 'link', 'xyz', 'syncro']
+print(results[0].orig_shape)
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+video_writer = cv2.VideoWriter('filename.avi', fourcc, 30, (1280, 720))
 
-for i, result in enumerate(results):
-    print('{}/690'.format(i))
-    img = result.orig_img.copy()
-    for boxes in result:
-        x1, y1, x2, y2, = map(int, boxes.boxes.xyxy.squeeze())
-        cv2.rectangle(img, (x1, y1), (x2, y2), color=(255, 0, 0), thickness=2)
+try:
+    for i, result in enumerate(results[0:]):
+        print(i)
+        img = result.orig_img.copy()
+        for boxes in result:
+            x1, y1, x2, y2, = map(int, boxes.boxes.xyxy.squeeze())
+            roi = img[y1:y2, x1:x2]
+            roi = roi.reshape((roi.shape[0] * roi.shape[1], 3)).astype(float)
+            cv2.rectangle(img, (x1, y1), (x2, y2), color=(255, 0, 0), thickness=2)
 
-        roi = img[y1:y2, x1:x2]
-        roi = roi.reshape((roi.shape[0] * roi.shape[1], 3))
-        kmeans.fit(roi)
+            # kmeans.fit(roi)
 
-        min = 500
-        label = ''
-        for cluster in kmeans.cluster_centers_:
-            for card_type, color in colors.items():
-                distance = np.linalg.norm(cluster - color)
-                if distance < min:
-                    min = distance
-                    label = card_type
+            # codes = kmeans.cluster_centers_
+            # print('cluster centres:\n', codes)
 
-        cv2.putText(img, label, (x1, y1), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 255, 0), 2)
+            counts = [
+                np.isclose(
+                    np.array([np.linalg.norm(diff) for diff in (roi - color)]),
+                    np.zeros(roi.shape[0]),
+                    atol=35
+                ).sum()
+                for color in colors
+            ]
 
-    print(img.shape)
-    video_writer.write(img)
-    cv2.imshow("", img)
-    cv2.waitKey()
-video_writer.release()
+            index_max = np.argmax(counts)  # find most frequent
+            card_type = card_types[index_max]
+            
+            # if len(result) > 1:
+            #     breakpoint()
+            # min = 500
+            # label = ''
+            # for cluster in kmeans.cluster_centers_:
+            # if len(result) > 1:
+            #     print("cluster", cluster, x1, y1, x2, y2)
+            # for card_type, color in colors.items():
+            #     distance = np.linalg.norm(colour - color)
+            #     # if len(result) > 1:
+            #     #     print(distance, card_type)
+            #     if distance < min:
+            #         min = distance
+            #         label = card_type
+
+            cv2.putText(img, card_type, (x1, y1), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 255, 0), 2)
+        video_writer.write(img)
+        # if len(result) > 1:
+        #     break
+        # print(img.shape)
+        # cv2.imshow("", img)
+        # cv2.waitKey()
+except KeyboardInterrupt:
+    video_writer.release()
