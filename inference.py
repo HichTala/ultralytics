02858813999785866281cda1bff11a11c1fdfc99
HichTala <98521878+HichTala@users.noreply.github.com>
2023-10-31 11:23:15 +0100

    Add classification part using beit

--- inference.py
+++ inference.py
@@ -1,6 +1,12 @@
+import os
+
 import cv2
 import numpy as np
+import torch
+from timm.models import create_model
+from torchvision import transforms
 
+import utils
 from ultralytics import YOLO
 
 
@@ -72,27 +78,65 @@ def get_lower_edges(coordinates, h, w):
         return None, None
 
 
-def crop_min_area_rect(img, rect):
+def crop_min_area_rect(img, box, angle):
     # rotate img
-    angle = rect[2]
+    # angle = rect[2]
     rows, cols = img.shape[0], img.shape[1]
     M = cv2.getRotationMatrix2D((cols / 2, rows / 2), angle, 1)
     img_rot = cv2.warpAffine(img, M, (cols, rows))
 
     # rotate bounding box
-    rect0 = (rect[0], rect[1], 0.0)
-    box = cv2.boxPoints(rect0)
+    # rect0 = (rect[0], rect[1], 0.0)
+    # box = cv2.boxPoints(rect0)
     pts = np.int0(cv2.transform(np.array([box]), M))[0]
     pts[pts < 0] = 0
+    # cv2.drawContours(img_rot, [pts], 0, (255, 152, 119), 2)
 
     # crop
-    img_crop = img_rot[pts[:, 0].min():pts[:, 0].max(), pts[:, 1].min():pts[:, 1].max()]
+    img_crop = img_rot[pts[:, 1].min():pts[:, 1].max(), pts[:, 0].min():pts[:, 0].max()]
     return img_crop
 
 
-model = YOLO("./runs/detect/train14/weights/best.pt")
+with open("test.ydk") as f:
+    ydk = [line.rstrip() for line in f.readlines()]
 
-results = model(
+directory = '/home/cose-ia/yugioh-card-classification/cardDatabaseFormatted/monster/'
+
+classes = [d.name for d in os.scandir(directory) if d.is_dir()]
+classes.sort()
+class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+
+deck_card_id = []
+
+for card_id in ydk:
+    if card_id[0] in '0123456789':
+        for card_name in class_to_idx.keys():
+            if card_id[0] == '0':
+                if card_id[1:] in card_name:
+                    deck_card_id.append(class_to_idx[card_name])
+            if card_id in card_name:
+                deck_card_id.append(class_to_idx[card_name])
+
+deck_card_id = list(set(deck_card_id))
+
+model_classification = create_model(
+    'beit_base_patch16_224',
+    pretrained=False,
+    num_classes=4602,
+    drop_rate=0,
+    drop_path_rate=0.1,
+    attn_drop_rate=0,
+    drop_block_rate=None,
+    use_rel_pos_bias=True,
+    use_abs_pos_emb=False,
+    init_values=0.1,
+)
+checkpoint = torch.load('/home/cose-ia/beit/runs/monster/checkpoint-best.pth')
+utils.load_state_dict(model_classification, checkpoint['model'])
+
+model_regression = YOLO("./runs/detect/train14/weights/best.pt")
+
+results = model_regression(
     source="/home/cose-ia/Downloads/YuGiOh_YOLO.v2i.yolov8/video/2023-07-02 16-26-49 (online-video-cutter.com).mp4",
     show_labels=False,
     save=False
@@ -181,44 +225,62 @@ try:
             cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
             min_area = 2000
-            max_area = 8000
-            for c in cnts:
-                area = cv2.contourArea(c)
-                # TODO: keep only max area rects
-                if min_area < area < max_area:
-                    rect = cv2.minAreaRect(c)
-                    box = cv2.boxPoints(rect)
-                    box = np.intp(box)
-                    cv2.drawContours(roi_original, [box], 0, (152, 255, 119), 2)
-                    edges1, edges2 = get_lower_edges(box, roi_original.shape[0], roi_original.shape[1])
-                    if edges1 is None:
-                        break
+            max_area = 4000
 
-                    dx1 = edges1[1][0] - edges1[0][0]
-                    dx2 = edges2[1][0] - edges2[0][0]
-                    dy1 = edges1[1][1] - edges1[0][1]
-                    dy2 = edges2[1][1] - edges2[0][1]
+            c = cnts[np.array(list(map(cv2.contourArea, cnts))).argmax()]
 
-                    dst1 = (dx1 ** 2 + dy1 ** 2) ** 0.5
-                    dst2 = (dx2 ** 2 + dy2 ** 2) ** 0.5
+            area = cv2.contourArea(c)
+            if min_area < area < max_area:
+                print(area)
+                rect = cv2.minAreaRect(c)
+                box = cv2.boxPoints(rect)
+                box = np.intp(box)
+                cv2.drawContours(roi_original, [box], 0, (152, 255, 119), 2)
+                edges1, edges2 = get_lower_edges(box, roi_original.shape[0], roi_original.shape[1])
+                if edges1 is None:
+                    break
 
-                    box_artwork = np.array([
-                        [edges1[1][0] + dx1 * 5 / dst1, edges1[1][1] + dy1 * 5 / dst1],
-                        [edges2[1][0] + dx2 * 5 / dst2, edges2[1][1] + dy2 * 5 / dst2],
-                        [edges2[1][0] + dx2 * 95 / dst2, edges2[1][1] + dy2 * 100 / dst2],
-                        [edges1[1][0] + dx1 * 95 / dst1, edges1[1][1] + dy1 * 100 / dst1],
-                    ], dtype=np.int64)
+                dx1 = edges1[1][0] - edges1[0][0]
+                dx2 = edges2[1][0] - edges2[0][0]
+                dy1 = edges1[1][1] - edges1[0][1]
+                dy2 = edges2[1][1] - edges2[0][1]
 
-                    contour2 = np.intp(cv2.boxPoints(cv2.minAreaRect(box_artwork)))
+                dst1 = (dx1 ** 2 + dy1 ** 2) ** 0.5
+                dst2 = (dx2 ** 2 + dy2 ** 2) ** 0.5
 
-                    artwork = crop_min_area_rect(roi_original, cv2.minAreaRect(box_artwork))
-                    cv2.drawContours(roi_original, [box_artwork], 0, (119, 152, 255), 2)
-                    # cv2.drawContours(roi_original, [contour2], 0, (119, 255, 152), 2)
-                    # if artwork.shape[0] != 0 and artwork.shape[1] != 0:
-                    #     cv2.imwrite(
-                    #         '/home/cose-ia/Downloads/YuGiOh_YOLO.v2i.yolov8/ROI/video-remote-artwork-1/frame_{}_box_{}.png'.format(frame, nbox),
-                    #         artwork
-                    #     )
+                box_artwork = np.array([
+                    [edges1[1][0] + dx1 * 5 / dst1, edges1[1][1] + dy1 * 5 / dst1],
+                    [edges2[1][0] + dx2 * 5 / dst2, edges2[1][1] + dy2 * 5 / dst2],
+                    [edges2[1][0] + dx2 * 95 / dst2, edges2[1][1] + dy2 * 100 / dst2],
+                    [edges1[1][0] + dx1 * 95 / dst1, edges1[1][1] + dy1 * 100 / dst1],
+                ], dtype=np.int64)
+
+                contour2 = np.intp(cv2.boxPoints(cv2.minAreaRect(box_artwork)))
+                # cv2.drawContours(roi_original, [box_artwork], 0, (119, 152, 255), 2)
+                artwork = crop_min_area_rect(roi_original, box_artwork,
+                                             180 - np.arctan(dx1 / dy1) * 180 / np.pi)
+
+                # cv2.drawContours(roi_original, [contour2], 0, (255, 152, 119), 2)
+                if artwork.shape[0] != 0 and artwork.shape[1] != 0:
+
+                    image = transforms.ToTensor()(artwork)
+                    image = transforms.Resize((224, 224), antialias=True)(image)
+                    image = transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])(image)
+                    model_classification.eval()
+                    output = model_classification(image.unsqueeze(0))
+
+                    _, indices = torch.sort(output, descending=True)
+
+                    for k, i in enumerate(indices[0]):
+                        if i in deck_card_id:
+                            cv2.imwrite(
+                                '/home/cose-ia/Downloads/YuGiOh_YOLO.v2i.yolov8/ROI/video-remote-artwork-1/frame_{}_box_{}_{}.png'.format(
+                                    frame, nbox, classes[i]),
+                                artwork
+                            )
+                            print(classes[i])
+                            print(k)
+                            break
 
         # cv2.putText(img, str(i), (10, 10), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 255), 2)
         video_writer.write(img)
